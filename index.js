204215fd19f012c6ac90b5870fe13cfae23b236e
mohamim360 <mohamim360@gmail.com>
2023-10-12 23:33:14 +0600

    comment

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
+// Import necessary libraries and modules
+
 const express = require("express");
-
 const bodyParser = require("body-parser");
-
 const path = require("path");
-
 const mongoose = require("mongoose");
-
-require("dotenv").config();
-
+require("dotenv").config(); // Load environment variables
 const cors = require("cors");
-
 const flash = require("connect-flash");
 
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// Import routes for different parts of the application
+const authRoutes = require("./routes/auth"); // Authentication routes
+const userRoutes = require("./routes/user"); // User-related routes
+const adminRoutes = require("./routes/admin"); // Admin-related routes
+const shopRoutes = require("./routes/shop"); // Shop-related routes
 
+// Create an instance of the Express application
 const app = express();
 
-app.use(bodyParser.json());
+// Middleware setup
+app.use(bodyParser.json()); // Parse JSON data in requests
+app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
+app.use(flash()); // Flash messages support
 
-app.use(cors());
+// Define route handlers for different parts of the application
+app.use("/auth", authRoutes); // Routes related to authentication
+app.use("/admin/user", userRoutes); // Routes for admin users
+app.use("/admin/product", adminRoutes); // Routes for admin products
+app.use("/shop", shopRoutes); // Routes for the shop
 
-app.use(flash());
-
-app.use("/auth", authRoutes);
-app.use("/admin/user", userRoutes);
-app.use("/admin/product", adminRoutes);
-app.use("/shop", shopRoutes);
-
+// Connect to the MongoDB database using the provided DB URI
 mongoose
   .connect(process.env.DB_URI)
   .then((result) => {
-    console.log("connected");
-    app.listen(5000);
+    console.log("Connected to the database");
+    app.listen(5000); // Start the Express application on port 5000
   })
   .catch((err) => {
-    console.log(err);
+    console.log("Error connecting to the database:", err);
   });
--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,38 @@
+// Import necessary libraries and modules
 const express = require("express");
-
 const adminController = require("../controllers/admin");
 const isAuth = require("../middleware/isAuth");
 const isAdmin = require("../middleware/isAdmin");
 
+// Create an instance of an Express Router
 const router = express.Router();
 
-router.post("/add-products", isAuth, isAdmin, adminController.postAddProduct);
+// Define routes for managing products, which require authentication and admin privileges
 
-router.get("/products", isAuth, isAdmin, adminController.getProducts);
+router.post("/add-products", isAuth, isAdmin, adminController.postAddProduct); // Add a new product
 
-router.get("/products/:prodId", isAuth, isAdmin, adminController.getEditProduct);
+router.get("/products", isAuth, isAdmin, adminController.getProducts); // Get a list of products
 
-router.delete("/products/:prodId", isAuth, isAdmin, adminController.deleteProduct);
+router.get(
+  "/products/:prodId",
+  isAuth,
+  isAdmin,
+  adminController.getEditProduct
+); // Get product details for editing
 
-router.put("/products/edit-product/:prodId", isAuth, isAdmin, adminController.putEditProduct)
+router.delete(
+  "/products/:prodId",
+  isAuth,
+  isAdmin,
+  adminController.deleteProduct
+); // Delete a product
 
+router.put(
+  "/products/edit-product/:prodId",
+  isAuth,
+  isAdmin,
+  adminController.putEditProduct
+); // Update product information
+
+// Export the router for use in other parts of the application
 module.exports = router;
